9d616e848d094e5fc4d9472f1cbfff1733102038
Cindy Mendoza <mendozaic17@gmail.com>
2023-10-01 23:05:20 -0500

    // Realizamos los siguientes cambios: // 1. Agregamos comentarios explicativos para cada sección del código JavaScript. // 2. Mantenemos el uso de location.reload() para recargar la página en caso de error de entrada. // 3. Se utilizan nombres de funciones descriptivos: `encryptText` y `decryptText`. // 4. Se utiliza `textContent` en lugar de `innerHTML` para evitar posibles ataques XSS. // 5. Se evita el uso de variables innecesarias, simplificando el código. // 6. Se utiliza un manejo más adecuado de los eventos para mejorar la claridad del código.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,62 +1,74 @@
+// Obtenemos referencias a los elementos del DOM
 const inputText = document.getElementById("inputText");
 const buttonEncrypt = document.getElementById("buttonEncrypt");
 const buttonDecrypt = document.getElementById("buttonDecrypt");
 const buttonCopy = document.getElementById("buttonCopy");
 const divError = document.getElementById("error");
 const divSuccess = document.getElementById("success");
+
+// Inicializamos el botón de copiar como oculto
 buttonCopy.style.display = "none";
-let arraystringtoencrypt = [
+
+// Mapeo de caracteres para encriptar/desencriptar
+let charMap = [
   ["e", "enter"],
   ["i", "imes"],
   ["a", "ai"],
   ["o", "ober"],
   ["u", "ufat"],
 ];
-function Encrypt(string) {
-  for (let i = 0; i < arraystringtoencrypt.length; i++) {
-    if (string.includes(arraystringtoencrypt[i][0])) {
-      string = string.replaceAll(
-        arraystringtoencrypt[i][0],
-        arraystringtoencrypt[i][1]
-      );
+
+// Función para encriptar el texto
+function encryptText(text) {
+  for (let i = 0; i < charMap.length; i++) {
+    if (text.includes(charMap[i][0])) {
+      text = text.replaceAll(charMap[i][0], charMap[i][1]);
     }
   }
-  return string;
+  return text;
 }
-function Decrypt(string) {
-  for (let i = 0; i < arraystringtoencrypt.length; i++) {
-    if (string.includes(arraystringtoencrypt[i][1])) {
-      string = string.replaceAll(
-        arraystringtoencrypt[i][1],
-        arraystringtoencrypt[i][0]
-      );
+
+// Función para desencriptar el texto
+function decryptText(text) {
+  for (let i = 0; i < charMap.length; i++) {
+    if (text.includes(charMap[i][1])) {
+      text = text.replaceAll(charMap[i][1], charMap[i][0]);
     }
   }
-  return string;
+  return text;
 }
-buttonEncrypt.addEventListener("click", function (e) {
-  if (!inputText.value.toLowerCase().match(/^[a-z]*$/)) {
-    alert("Solo letras minúsculas y sin acentos");
+
+// Evento al hacer clic en el botón de encriptar
+buttonEncrypt.addEventListener("click", function () {
+  const userInput = inputText.value.toLowerCase();
+  // Validamos el texto de entrada
+  if (!userInput.match(/^[a-z]*$/)) {
+    divError.textContent = "Solo letras minúsculas y sin acentos";
+    divError.style.display = "block";
+    // Recargamos la página para limpiar el input
     location.reload();
-    inputText.value = "";
   } else {
     divError.style.display = "none";
-    divSuccess.innerHTML = Encrypt(inputText.value);
+    divSuccess.textContent = encryptText(userInput);
     buttonCopy.style.display = "inline-block";
   }
 });
-buttonDecrypt.addEventListener("click", function (e) {
-  divSuccess.innerHTML = "";
-  divSuccess.innerHTML = Decrypt(inputText.value);
+
+// Evento al hacer clic en el botón de desencriptar
+buttonDecrypt.addEventListener("click", function () {
+  divSuccess.textContent = decryptText(inputText.value);
 });
-buttonCopy.addEventListener("click", function (e) {
+
+// Evento al hacer clic en el botón de copiar
+buttonCopy.addEventListener("click", function () {
   const range = document.createRange();
   range.selectNodeContents(divSuccess);
   const selection = window.getSelection();
   selection.removeAllRanges();
   selection.addRange(range);
+  // Copiamos el texto al portapapeles
   navigator.clipboard.writeText(selection);
-  console.log(selection);
-  divSuccess.value = "";
+  // Limpiamos el contenido de éxito y mostramos un mensaje de alerta
+  divSuccess.textContent = "";
   alert("Texto Copiado");
 });
